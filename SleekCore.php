2c58f1228e298c0902e5557c6a924bd1f2d761f6
AL <powerbuoy@gmail.com>
2019-09-12 10:55:38 +0200

    Moar

--- SleekCore.php
+++ SleekCore.php
@@ -26,9 +26,22 @@ add_filter('use_block_editor_for_post_type', '__return_false', 10);
 ##################################
 # Show the editor on the blog page
 # https://wordpress.stackexchange.com/questions/193755/show-default-editor-on-blog-page-administration-panel
-// add_action('add_meta_boxes', function ($post_type, $post) {
-// 	if (isset($post->ID) and $post->ID === get_option('page_for_posts')) {
-// 		remove_action('edit_form_after_title', '_wp_posts_page_notice');
-// 		add_post_type_support('page', 'editor');
-// 	}
-// }, 0, 2);
+add_action('edit_form_after_title', function ($post) {
+	if ($post->ID === get_option('page_for_posts')) {
+		add_post_type_support('page', 'editor');
+	}
+}, 0);
+
+#####################
+# Change email sender
+add_filter('wp_mail_from', 'sleek_email_from');
+
+function sleek_email_from () {
+	return get_option('admin_email');
+}
+
+add_filter('wp_mail_from_name', 'sleek_email_from_name');
+
+function sleek_email_from_name () {
+	return get_bloginfo('name');
+}
