f6d0323ab69b6bae6111dfc2bbe570edac69dfdc
AL <powerbuoy@gmail.com>
2019-09-13 14:49:23 +0200

    Moar

--- sleek-core.php
+++ sleek-core.php
@@ -10,9 +10,11 @@ add_action('wp_head', function () {
 
 ###############
 # Theme support
+add_theme_support('html5');
 add_theme_support('title-tag');
 add_theme_support('custom-logo');
 add_theme_support('post-thumbnails');
+add_theme_support('automatic-feed-links');
 
 ###################
 # Disable Gutenberg
